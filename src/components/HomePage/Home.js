da15c650e2497fd59c44a2b098fac07df74a71ba
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2024-07-27 00:16:48 +0530

    Added a loading Spinner

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,30 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Heading from "../Home/Heading";
+import Loader from "../UI/Loader";
+import "./Home.css";
 
 const Home = () => {
-  const [movies,setMovies]=useState([]);
-  useEffect(() => {
-    fetchMoviesData();
-  }, []);
-  const fetchMoviesData = async () => {
-    try{
-      const data = await fetch("https://swapi.dev/api/films");
-      const  jsonData= await data.json();
-      setMovies(jsonData?.results);
-      
+  const [movies, setMovies] = useState([]);
+  const [loading, setSetLoading] = useState(false);
 
-    }catch(error){
-      console.log(error)
+  const fetchMoviesData = async () => {
+    setSetLoading(true);
+    try {
+      const data = await fetch("https://swapi.dev/api/films");
+      const jsonData = await data.json();
+
+      setMovies(jsonData?.results);
+      setSetLoading(false);
+    } catch (error) {
+      console.log(error);
     }
-    
   };
+
   return (
     <div>
       <Heading />
-      
-      <main className="container mt-4">
-        <h1 className="text-dark mb-4 text-center">Tours</h1>
-        <table className="table ">
+
+      <main className="container ">
+        <h1 className="text-dark mb-4 text-center">Movies</h1>
+        <button onClick={fetchMoviesData}>Fetch Movies</button>
+
+        <table className="table">
           <thead>
             <tr>
               <th>Release Date</th>
@@ -33,18 +37,26 @@ const Home = () => {
               <th>Action</th>
             </tr>
           </thead>
-          <tbody>
-            {movies.map((data) => (
-              <tr key={data.episode_id}>
-                <td>{data.release_date}</td>
-                <td>{data.title}</td>
-                <td>{data.director}</td>
-                <td>
-                  <button className="btn btn-primary">BUY TICKETS</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          {loading && (
+            <div className="loader-center">
+              <Loader />
+            </div>
+          )}
+
+          {!loading && (
+            <tbody>
+              {movies.map((data) => (
+                <tr key={data.episode_id}>
+                  <td>{data.release_date}</td>
+                  <td>{data.title}</td>
+                  <td>{data.director}</td>
+                  <td>
+                    <button className="btn btn-primary">BUY TICKETS</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
         </table>
       </main>
     </div>
