87f9d7b2578c29e74f9aaf93625e870cbc35f6eb
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2024-07-26 23:06:48 +0530

    Fetchd Data form Api

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,36 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Heading from "../Home/Heading";
 
 const Home = () => {
-  const ticketData = [
-    { id: 1, Date: "Jul 17", location: "DETROIT, MI,", place: "DTE ENERGY MUSIC THEATRE" },
-    { id: 2, Date: "Jul 19", location: "TORONTO,ON", place: "BUDWEISER STAGE" },
-    { id: 3, Date: "Jul 27", location: "BRISTOW, VA", place: "JIGGY LUBE LIVE" },
-    { id: 4, Date: "Aug 05", location: "PHOENIX, AZ", place: "AK-CHIN PAVILION" },
-    { id: 5, Date: "Aug 10", location: "LAS VEGAS, NV", place: "T-MOBILE ARENA" },
-    { id: 6, Date: "Aug 17", location: "CONCORD, CA", place: "CONCORD PAVILION" },
-  ];
+  const [movies,setMovies]=useState([]);
+  useEffect(() => {
+    fetchMoviesData();
+  }, []);
+  const fetchMoviesData = async () => {
+    try{
+      const data = await fetch("https://swapi.dev/api/films");
+      const  jsonData= await data.json();
+      setMovies(jsonData?.results);
+      
 
+    }catch(error){
+      console.log(error)
+    }
+    
+  };
   return (
     <div>
       <Heading />
+      
       <main className="container mt-4">
-        <h1 className="text-dark mb-4 text-center" >Tours</h1>
+        <h1 className="text-dark mb-4 text-center">Tours</h1>
         <table className="table ">
           <thead>
             <tr>
-              <th>Date</th>
-              <th>Location</th>
-              <th>Place</th>
+              <th>Release Date</th>
+              <th>Title</th>
+              <th>Director</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {ticketData.map((data) => (
-              <tr key={data.id}>
-                <td>{data.Date}</td>
-                <td>{data.location}</td>
-                <td>{data.place}</td>
+            {movies.map((data) => (
+              <tr key={data.episode_id}>
+                <td>{data.release_date}</td>
+                <td>{data.title}</td>
+                <td>{data.director}</td>
                 <td>
                   <button className="btn btn-primary">BUY TICKETS</button>
                 </td>
